79257d8a12b58697b458a638bd1132da4d4f7084
Raphael Peters <rappet@rappet.de>
2020-06-29 13:23:38 +0200

    Changed ptrs table

--- db/migrations/000001_create_ptrs_table.down.sql
+++ db/migrations/000001_create_ptrs_table.down.sql
@@ -1,2 +1,3 @@
+DROP INDEX idx_ptrs_domain;
 DROP INDEX trgm_idx_ptrs_record;
 DROP TABLE ptrs;
--- db/migrations/000001_create_ptrs_table.up.sql
+++ db/migrations/000001_create_ptrs_table.up.sql
@@ -1,9 +1,11 @@
 CREATE TABLE ptrs (
     address VARCHAR(15) PRIMARY KEY,
     record VARCHAR,
-    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    domain VARCHAR GENERATED ALWAYS AS (lower(substring(record from '[\w-]+\.[\w-]+$'))) STORED
 );
 
 CREATE EXTENSION pg_trgm;
 
 CREATE INDEX trgm_idx_ptrs_record ON ptrs USING gin (record gin_trgm_ops);
+CREATE INDEX idx_ptrs_domain ON ptrs(domain);
